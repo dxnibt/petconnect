dfa78e0615599f2db1cee547d21b01500bed3be6
dxnibt <dxni0801@gmail.com>
2025-11-13 20:37:56 -0500

    Agrego y modifico atributos en la clase Usuario

--- auth/src/main/java/com/petconnect/auth/domain/model/Usuario.java
+++ auth/src/main/java/com/petconnect/auth/domain/model/Usuario.java
@@ -1,11 +1,12 @@
 package com.petconnect.auth.domain.model;
 
+import com.petconnect.auth.domain.model.nums.usuario.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,14 +16,15 @@ import java.time.LocalDate;
 public class Usuario {
 
     private Long id;
-    private String nombre;
+    private String name;
     private String email;
     private String password;
-    private LocalDate fechaNacimiento;
-    private String telefono;
-    private String ciudad;
-    private String direccion;
-    private String fotoPerfil;
-    private String role;
+    private String phoneNumber;
+    private String city;
+    private String address;
+    private String profilePicture;
+    private Role role;
+    private LocalDateTime registrationDate;
+    private LocalDateTime lastLogin;
 
 }
