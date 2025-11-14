85e7c808bc39a53e44e2ac7002d1bd54ec9fe44b
dxnibt <dxni0801@gmail.com>
2025-11-13 20:42:12 -0500

    Agrego clase Refugio que hereda de Usuario

---
+++ auth/src/main/java/com/petconnect/auth/domain/model/Refugio.java
@@ -0,0 +1,19 @@
+package com.petconnect.auth.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Refugio extends Usuario{
+
+    private String nit;
+    private String website;
+    private Integer currentPets;
+    private String supportDocument;
+
+}
