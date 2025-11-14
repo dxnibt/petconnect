153ec91bbed307eeec9550a4c33dbbbeed8054f4
AndressGarzon <andresgarzon0422@gmail.com>
2025-11-14 10:39:45 -0500

    configuración sqs, sns, ses (falta credenciales)

--- notification/src/main/java/com/ecommerce/notification/infraestructure/sqs/SqsListener.java
+++ notification/src/main/java/com/ecommerce/notification/infraestructure/sqs/SqsListener.java
@@ -1,4 +1,59 @@
 package com.ecommerce.notification.infraestructure.sqs;
 
+import com.ecommerce.notification.infraestructure.ses.SesEmailSender;
+import com.ecommerce.notification.infraestructure.sns.SnsSmsSender;
+import com.ecommerce.notification.infraestructure.sqs.dto.EventoNotificacionDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+
+@Component
+@RequiredArgsConstructor
 public class SqsListener {
+
+    private final SqsClient sqsClient;
+    private final ObjectMapper objectMapper;
+    private final SnsSmsSender smsSender;
+    private final SesEmailSender emailSender;
+
+    private final String QUEUE_URL = "URL-COLA";
+
+    @PostConstruct
+    public void escucharMensajes() {
+        Executors.newSingleThreadExecutor().submit(() -> {
+            while (true) {
+                ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
+                        .queueUrl(QUEUE_URL)
+                        .maxNumberOfMessages(5)
+                        .waitTimeSeconds(10)
+                        .build();
+
+                List<Message> messages = sqsClient.receiveMessage(receiveRequest).messages();
+
+                for (Message message : messages) {
+                    try {
+                        EventoNotificacionDTO evento = objectMapper.readValue(message.body(), EventoNotificacionDTO.class);
+
+                        smsSender.enviarSms(evento.getMessage(), evento.getPhoneNumber());
+                        emailSender.enviarEmail(evento.getEmail(), evento.getType(), evento.getMessage());
+
+                        sqsClient.deleteMessage(DeleteMessageRequest.builder()
+                                .queueUrl(QUEUE_URL)
+                                .receiptHandle(message.receiptHandle())
+                                .build());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
 }
--- notification/src/main/java/com/ecommerce/notification/infraestructure/sqs/dto/EventoNotificacionDTO.java
+++ notification/src/main/java/com/ecommerce/notification/infraestructure/sqs/dto/EventoNotificacionDTO.java
@@ -1,5 +1,17 @@
 package com.ecommerce.notification.infraestructure.sqs.dto;
 
-public class EventoNotificacionDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventoNotificacionDTO {
+    private String type;
+    private String email;
+    private String phoneNumber;
+    private String message;
 }
