99506cff96552acd1a641cbbef5256fdfbf339ed
itsm31416 <paula02pal@gmail.com>
2025-11-15 17:15:29 -0500

    Eliminado método personalizado y corrección del gateway

--- pets/src/main/java/com/petconnect/pets/domain/model/gateway/MascotaGateway.java
+++ pets/src/main/java/com/petconnect/pets/domain/model/gateway/MascotaGateway.java
@@ -7,9 +7,9 @@ import java.util.List;
 public interface MascotaGateway {
 
     Mascota guardar(Mascota mascota);
-    Mascota buscarPorId(Long id_mascota);
+    Mascota buscarPorId(Long pet_id);
     List<Mascota> obtenerTodas(int page, int size);
     Mascota actualizarMascota(Mascota mascota);
-    void eliminar(Long id_mascota);
+    void eliminar(Long pet_id);
 
 }
--- pets/src/main/java/com/petconnect/pets/infraestructure/driver_adapters/jpa_repository/MascotaDataJpaRepository.java
+++ pets/src/main/java/com/petconnect/pets/infraestructure/driver_adapters/jpa_repository/MascotaDataJpaRepository.java
@@ -2,9 +2,6 @@ package com.petconnect.pets.infraestructure.driver_adapters.jpa_repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MascotaDataJpaRepository extends JpaRepository <MascotaData, Long> {
-    Optional<MascotaData> findByName(String name);
-    Optional<MascotaData> findBySpecies(String species);
+
 }
