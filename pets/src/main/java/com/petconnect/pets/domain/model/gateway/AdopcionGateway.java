18464bece3e733a2c698e81e63aa3910cb342626
AndressGarzon <andresgarzon0422@gmail.com>
2025-11-15 19:18:27 -0500

    Se agrega clase Adopcion, Gateway y Data

--- pets/src/main/java/com/petconnect/pets/domain/model/gateway/AdopcionGateway.java
+++ pets/src/main/java/com/petconnect/pets/domain/model/gateway/AdopcionGateway.java
@@ -1,4 +1,12 @@
 package com.petconnect.pets.domain.model.gateway;
 
+import com.petconnect.pets.domain.model.Adopcion;
+
 public interface AdopcionGateway {
+    Adopcion crear(Adopcion adopcion);
+    Adopcion buscarPorUserId(Long userId);
+    Adopcion buscarPorShelterId(Long shelterId);
+    Adopcion obtenerAdopcionPorId(Long adoptionId);
+    Adopcion actualizar(Adopcion adopcion);
+    void eliminarAdopcion(Long adoptionId);
 }
--- pets/src/main/java/com/petconnect/pets/infraestructure/driver_adapters/jpa_repository/adopcion/AdopcionData.java
+++ pets/src/main/java/com/petconnect/pets/infraestructure/driver_adapters/jpa_repository/adopcion/AdopcionData.java
@@ -1,4 +1,29 @@
 package com.petconnect.pets.infraestructure.driver_adapters.jpa_repository.adopcion;
 
+import com.petconnect.pets.domain.model.enums.EstadoAdopcion;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "adopciones")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class AdopcionData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAdoption;
+    private Long userId;
+    private Long petId;
+    private Long shelterId;
+    private String requestDate;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoAdopcion status;
+    private String responseDate;
+
 }
