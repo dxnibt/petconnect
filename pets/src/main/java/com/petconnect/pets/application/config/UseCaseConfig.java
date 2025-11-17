dafd1d115e0388af90b1b53518408a9e1a84863a
itsm31416 <paula02pal@gmail.com>
2025-11-16 20:13:04 -0500

    Agregado el caso de uso de adopciones en el Bean

--- pets/src/main/java/com/petconnect/pets/application/config/UseCaseConfig.java
+++ pets/src/main/java/com/petconnect/pets/application/config/UseCaseConfig.java
@@ -1,6 +1,9 @@
 package com.petconnect.pets.application.config;
 
+import com.petconnect.pets.domain.model.Adopcion;
+import com.petconnect.pets.domain.model.gateway.AdopcionGateway;
 import com.petconnect.pets.domain.model.gateway.MascotaGateway;
+import com.petconnect.pets.domain.usecase.AdopcionUseCase;
 import com.petconnect.pets.domain.usecase.MascotaUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,10 @@ public class UseCaseConfig {
     @Bean
     public MascotaUseCase mascotaUseCase(MascotaGateway mascotaGateway){
         return new MascotaUseCase(mascotaGateway);
+    }
+    @Bean
+    public AdopcionUseCase adopcionUseCase(AdopcionGateway adopcionGateway){
+        return new AdopcionUseCase(adopcionGateway);
     }
 
 }
