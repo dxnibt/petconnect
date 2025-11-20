550bc32ef68483588e581af4ad26b17418b99ffe
dxnibt <dxni0801@gmail.com>
2025-11-19 21:03:20 -0500

    Se corrigen aspectos de JWT

--- notification/src/main/java/com/ecommerce/notification/application/config/AwsConfig.java
+++ notification/src/main/java/com/ecommerce/notification/application/config/AwsConfig.java
@@ -19,8 +19,8 @@ public class AwsConfig {
                 .credentialsProvider(
                         StaticCredentialsProvider.create(
                                 AwsBasicCredentials.create(
-                                        "accessKeyId",
-                                        "secretAcessKey"
+                                        "",
+                                        ""
                                 )
                         )
                 )                .build();
@@ -33,8 +33,8 @@ public class AwsConfig {
                 .credentialsProvider(
                         StaticCredentialsProvider.create(
                                 AwsBasicCredentials.create(
-                                        "accessKeyId",
-                                        "secretAcessKey"
+                                        "",
+                                        ""
                                 )
                         )
                 )                .build();
@@ -46,8 +46,8 @@ public class AwsConfig {
                 .credentialsProvider(
                         StaticCredentialsProvider.create(
                                 AwsBasicCredentials.create(
-                                        "accessKeyId",
-                                        "secretAccessKey"
+                                        "",
+                                        ""
                                 )
                         )
                 )                .build();
