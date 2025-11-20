f56fe05d44e0ecdc64681c5b2c60a2a25f18c620
itsm31416 <paula02pal@gmail.com>
2025-11-20 13:03:00 -0500

    Se corrige securityConfig

--- pets/src/main/java/com/petconnect/pets/infrastructure/security/JwtFilter.java
+++ pets/src/main/java/com/petconnect/pets/infrastructure/security/JwtFilter.java
@@ -23,7 +23,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
+        System.out.println("JwtFilter ejecutándose para URL: " + request.getRequestURI());
         String header = request.getHeader("Authorization");
         if (header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
