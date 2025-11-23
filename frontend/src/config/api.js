8be44d8b5a88a530396cb56e4bf13198ed6c6dc5
AndressGarzon <andresgarzon0422@gmail.com>
2025-11-23 16:10:29 -0500

    Configuración extra de front

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,16 +1,8 @@
 const API_CONFIG = {
-  development: {
-    AUTH_URL: 'http://localhost:8181',
-    PETS_URL: 'http://localhost:9494',
-    CHATBOT_URL: 'http://localhost:9090',
-    NOTIFICATION_URL: 'http://localhost:9092'
-  },
-  production: {
-    AUTH_URL: 'https://auth-service-cgdc.onrender.com',
-    PETS_URL: 'https://pets-service-gbdc.onrender.com',
-    CHATBOT_URL: 'https://chatbot-service-59lk.onrender.com',
-    NOTIFICATION_URL: 'https://notification-service.onrender.com'
-  }
+  AUTH_URL: import.meta.env.VITE_AUTH_URL,
+  PETS_URL: import.meta.env.VITE_PETS_URL,
+  CHATBOT_URL: import.meta.env.VITE_CHATBOT_URL,
+  NOTIFICATION_URL: import.meta.env.VITE_NOTIFICATION_URL
 };
 
-export default API_CONFIG[process.env.NODE_ENV || 'development'];
+export default API_CONFIG;
